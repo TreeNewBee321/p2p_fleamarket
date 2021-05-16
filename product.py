1e77f473203b5a78066af05058988033681a4dd8
grenadier56 <zy1546@nyu.edu>
2021-05-16 18:43:26 -0400

    add email

--- product.py
+++ product.py
@@ -8,6 +8,7 @@ class Product:
             self.price = 0
             self.owner = ''
             self.phone = ''
+            self.email = ''
         else:
             self.uid = jf['UID']
             self.name = jf['Name']
@@ -15,6 +16,7 @@ class Product:
             self.price = int(jf['Price'])
             self.owner = jf['Owner']
             self.phone = jf['Phone']
+            self.phone = jf['Email']
     #end def __init__
 
 
@@ -27,7 +29,7 @@ class Product:
         #print('Price:', self.price)
         #print('Description:', self.description)
         #print('Owner:', self.owner)
-        print(f'{self.uid}\t{self.name}\t{self.price}\t{self.owner}\t{self.phone}\t{self.description}')
+        print(f'{self.uid}\t{self.name}\t{self.price}\t{self.owner}\t{self.phone}\t{self.email}\t{self.description}')
     #end def printInfo
 
     def jsonFile(self):
@@ -36,6 +38,7 @@ class Product:
                 'Price': self.price,
                 'Description': self.description,
                 'Phone': self.phone,
+                'Email': self.email,
                 'Owner': self.owner}
 
 #end class
